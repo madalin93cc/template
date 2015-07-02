023efdd36d59e577b80afdbb05caa2950e4d3309
Madalin.Colezea <12345@qwert>
2015-07-02 17:00:30 +0300

    refactorizare client side

--- src/main/resources/static/js/hello.js
+++
@@ -1,9 +0,0 @@
-angular.module('hello', [])
-    .controller('home', function($scope, $http) {
-        $scope.addPerson = function(){
-            $http.post('/add/person', $scope.personDTO).success(function(response) {
-                console.log(response);
-            }).error(function() {
-            });
-        }
-    })
---
+++ src/main/resources/static/js/home-controller.js
@@ -0,0 +1,8 @@
+app.controller('home', function($scope, $http) {
+    $scope.addPerson = function(){
+        $http.post('/add/person', $scope.personDTO).success(function(response) {
+            console.log(response);
+        }).error(function() {
+        });
+    }
+});
